70e567adab1892fe777b775da99cf3369715dc9e
Héber Lima Silva <heberlimasilva@gmail.com>
2021-10-16 11:35:26 -0300

    commit

--- ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
+++ ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -1,35 +1,21 @@
 import React, { Component } from "react";
+import CriarNota from "../CriarNota/CriarNota";
 
-export default class ListaDeNotas extends Component {
+class ListaDeNotas extends Component {
   render() {
     return (
       <ul>
         <li>
-          <section>
-            <header>
-              <h3>Titulo</h3>
-            </header>
-            <p>Escreva sua nota</p>
-          </section>
+          <CriarNota />
         </li>
         <li>
-          <section>
-            <header>
-              <h3>Titulo</h3>
-            </header>
-            <p>Escreva sua nota</p>
-          </section>
+          <CriarNota />
         </li>
         <li>
-          <section>
-            <header>
-              <h3>Titulo</h3>
-            </header>
-            <p>Escreva sua nota</p>
-          </section>
+          <CriarNota />
         </li>
       </ul>
     );
   }
 }
-
+export default ListaDeNotas;
