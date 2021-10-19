dda33c391625f5e0241c935b4eecfe977344f382
Héber Lima Silva <heberlimasilva@gmail.com>
2021-10-19 09:24:11 -0300

    commit

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -5,11 +5,31 @@ import "./Assets/App.css";
 import "./Assets/index.css";
 
 class App extends Component {
+  
+  constructor(){
+    super();
+    
+    this.state = {
+      notas: [],
+    }
+  }
+
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto}
+    
+    const novoArrayNotas = [...this.state.notas, novaNota]
+    
+    const novoEstado = {
+      notas:novoArrayNotas
+    }
+    this.setState(novoEstado)
+  }
+
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota = {this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas}/>
       </section>
     );
   }
